3c22ee60d45da6a78548b4c11a070db3202a79f1
azuscreamo <azuscreamo2003@gmail.com>
2025-10-30 10:19:53 -0300

    2

--- src/main/java/modelo/Biblioteca.java
+++ src/main/java/modelo/Biblioteca.java
@@ -8,6 +8,90 @@ package modelo;
  *
  * @author Santo Tomas
  */
-public class Biblioteca {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca{
+    private int id;
+    private List<Libro> libros;
     
-}
+     public Biblioteca(int id) {
+        this.id = id;
+        this.libros = new ArrayList<>(); 
+    }
+
+    public Biblioteca() {
+    }
+
+    public Biblioteca(int id, List<Libro> libros) {
+        this.id = id;
+        this.libros = libros;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(List<Libro> libros) {
+        this.libros = libros;
+    }
+     
+    public void agregarLibros(Libro libro){
+        if(libro == null){
+            throw new IllegalArgumentException("No se puede agregar un libro nulo.");
+        }
+        this.libros.add(libro);
+        System.out.println("Libro agregado: " + libro.getTitulo());
+    }
+    
+    public List<Libro> listarLibros() {
+        return this.libros;
+    }
+    
+    public void actualizarLibro(int idLibro, String nuevoTitulo, int nuevoAnio, Autor nuevoAutor) {
+        // Validación 1: Buscar si el libro existe.
+        Libro libroParaActualizar = buscarLibroPorId(idLibro);
+        
+        if (libroParaActualizar == null) {
+            throw new IllegalArgumentException("No se encontró el libro con ID " + idLibro + " para actualizar.");
+        }
+        
+        try {
+            libroParaActualizar.setTitulo(nuevoTitulo);
+            libroParaActualizar.setAniopublicacion(nuevoAnio);
+            libroParaActualizar.setAutor(nuevoAutor);
+            System.out.println("Libro actualizado: " + libroParaActualizar.getTitulo());
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error al actualizar: " + e.getMessage());
+        }
+    }
+    
+    public void eliminarLibro(int idLibro) {
+        Libro libroParaEliminar = buscarLibroPorId(idLibro);
+        
+        if (libroParaEliminar == null) {
+            throw new IllegalArgumentException("No se encontró el libro con ID " + idLibro + " para eliminar.");
+        }
+
+        this.libros.remove(libroParaEliminar);
+        System.out.println("Libro eliminado: " + libroParaEliminar.getTitulo());
+    }
+    
+    public Libro buscarLibroPorId(int id) {
+        for (Libro libro : libros) {
+            if (libro.getId() == id) {
+                return libro; // Lo encontró
+            }
+        }
+        return null; // No lo encontró
+    }
+    
+}   
