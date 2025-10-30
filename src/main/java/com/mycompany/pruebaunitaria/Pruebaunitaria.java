99d19f4b88589d01aa43889f0247264b5779fadd
azuscreamo <azuscreamo2003@gmail.com>
2025-10-30 10:43:02 -0300

    3

--- src/main/java/com/mycompany/pruebaunitaria/Pruebaunitaria.java
+++ src/main/java/com/mycompany/pruebaunitaria/Pruebaunitaria.java
@@ -3,14 +3,22 @@
  */
 
 package com.mycompany.pruebaunitaria;
+import modelo.Biblioteca;
+import modelo.Autor;
+import modelo.Libro;
 
 /**
  *
  * @author Santo Tomas
  */
 public class Pruebaunitaria {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Biblioteca miBiblioteca = new Biblioteca(1);
+        
+        Autor autor1 = new Autor(1, "Gabriel García Márquez", "Colombiano");
+        Autor autor2 = new Autor(2, "Isabel Allende", "Chilena");
+        Libro libro1 = new Libro(101, "Cien años de soledad", 1967, autor1);
+        Libro libro2 = new Libro(102, "La casa de los espíritus", 1982, autor2);
     }
+
 }
