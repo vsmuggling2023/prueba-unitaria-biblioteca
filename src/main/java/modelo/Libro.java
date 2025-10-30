6106d4e9f8350df0996d74b389e34f060fe9158b
Victor Perez <144077516+vsmuggling2023@users.noreply.github.com>
2025-10-30 09:17:36 -0300

    Se establece nuevamente los constructores, get y set

--- src/main/java/modelo/Libro.java
+++ src/main/java/modelo/Libro.java
@@ -8,6 +8,57 @@ package modelo;
  *
  * @author Santo Tomas
  */
-public class Libro {
+public class Libro{
+    private int id;
+    private String titulo; //(3, 20)
+    private int aniopublicacion; //(4)
+    private Autor autor;
+    public Libro() {
+    }
+
+    public Libro(int id, String titulo, int aniopublicacion, Autor autor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.aniopublicacion = aniopublicacion;
+        this.autor = autor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAniopublicacion() {
+        return aniopublicacion;
+    }
+
+    public void setAniopublicacion(int aniopublicacion) {
+        this.aniopublicacion = aniopublicacion;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" + "id=" + id + ", titulo=" + titulo + ", aniopublicacion=" + aniopublicacion + ", autor=" + autor + '}';
+    }
+
     
 }
