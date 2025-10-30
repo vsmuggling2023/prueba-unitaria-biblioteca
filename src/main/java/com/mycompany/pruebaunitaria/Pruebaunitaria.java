e55fd2397eedd5ce926975115f983b33bc2883fa
Victor Perez <144077516+vsmuggling2023@users.noreply.github.com>
2025-10-30 08:57:45 -0300

    Prototipo

---
+++ src/main/java/com/mycompany/pruebaunitaria/Pruebaunitaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebaunitaria;
+
+/**
+ *
+ * @author Santo Tomas
+ */
+public class Pruebaunitaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
